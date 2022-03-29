b792f2a667b113e147f561d3c8763357302365e8
Marat <Maratik79@sibmail.com>
2022-03-29 11:33:43 +0700

    добавили параметр и вывели его в консоль

--- src/Stone.java
+++ src/Stone.java
@@ -1,18 +1,21 @@
 public class Stone {
-    public int weight;
-    public String color;
+    int weight;
+    String color;
+    int facetsOfStone;
 
-    public Stone(int weight, String color) {
+    public Stone(int weight, String color, int facetsOfStone) {
         this.weight = weight;
         this.color = color;
+        this.facetsOfStone= facetsOfStone;
     }
 
     public void printWeight() {
         System.out.println("Вес камня = " + weight);
         System.out.println("Цвет камня: " + color);
+        System.out.println("Количество граней камня = " + facetsOfStone);
     }
     public static void main(String[] args) {
-        Stone stone = new Stone(34, "red");
+        Stone stone = new Stone(34, "red", 42);
         stone.printWeight();
 
     }
